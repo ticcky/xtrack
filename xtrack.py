bda5c09769a77c9f67ab7552e757738c81fbef1f
Lukas Zilka <lukas@zilka.me>
2014-11-04 16:46:43 +0100

    Variable seq length.

--- xtrack.py
+++ xtrack.py
@@ -1,4 +1,6 @@
 import os
+#os.environ['THEANO_FLAGS'] = 'optimizer=None,exception_verbosity=high,\
+# linker=py'
 os.environ['THEANO_FLAGS'] = 'optimizer=None,exception_verbosity=high'
 
 import climate
@@ -9,66 +11,66 @@ import random
 import theano
 import theano.gradient
 from theano import (function, tensor as tt)
+from theano import printing
 
 
 def initial_weights(*argv):
-	return np.asarray(
-		np.random.uniform(
-			low  = -np.sqrt(6. / sum(argv)),
-			high =  np.sqrt(6. / sum(argv)),
-			size =  argv
-		),
-		dtype=theano.config.floatX
-	)
-
+    return np.asarray(
+        np.random.uniform(
+            low  = -np.sqrt(6. / sum(argv)),
+            high =  np.sqrt(6. / sum(argv)),
+            size =  argv
+        ),
+        dtype=theano.config.floatX
+    )
 
 
 class Parameters():
-	def __init__(self):
-		self.__dict__['params'] = {}
+    def __init__(self):
+        self.__dict__['params'] = {}
 
-	def __setattr__(self,name,array):
-		params = self.__dict__['params']
-		if name not in params:
-			params[name] = theano.shared(
-				value = np.asarray(
-					array,
-					dtype = np.float32
-				),
-				name = name
-			)
-		else:
-			print "%s already assigned"%name
-			params[name].set_value(np.asarray(
-					array,
-					dtype = np.float32
-				))
-	def __setitem__(self,name,array):
-		self.__setattr__(name,array)
-	def __getitem__(self,name):
-		return self.__getattr__(name)
+    def __setattr__(self,name,array):
+        params = self.__dict__['params']
+        if name not in params:
+            params[name] = theano.shared(
+                value = np.asarray(
+                    array,
+                    dtype = np.float32
+                ),
+                name = name
+            )
+        else:
+            print "%s already assigned"%name
+            params[name].set_value(np.asarray(
+                    array,
+                    dtype = np.float32
+                ))
+    def __setitem__(self,name,array):
+        self.__setattr__(name,array)
+    def __getitem__(self,name):
+        return self.__getattr__(name)
 
-	def __getattr__(self,name):
-		params = self.__dict__['params']
-		return self.params[name]
+    def __getattr__(self,name):
+        params = self.__dict__['params']
+        return self.params[name]
 
-	def remove(self,name):
-		del self.__dict__['params'][name]
+    def remove(self,name):
+        del self.__dict__['params'][name]
 
 
-	def values(self):
-		params = self.__dict__['params']
-		return params.values()
+    def values(self):
+        params = self.__dict__['params']
+        return params.values()
 
-	def save(self,filename):
-		params = self.__dict__['params']
-		pickle.dump({p.name:p.get_value() for p in params.values()},open(filename,'wb'),2)
+    def save(self,filename):
+        params = self.__dict__['params']
+        pickle.dump({p.name:p.get_value() for p in params.values()},open(filename,'wb'),2)
 
-	def load(self,filename):
-		params = self.__dict__['params']
-		loaded = pickle.load(open(filename,'rb'))
-		for k in params:
-			params[k].set_value(loaded[k])
+    def load(self,filename):
+        params = self.__dict__['params']
+        loaded = pickle.load(open(filename,'rb'))
+        for k in params:
+            params[k].set_value(loaded[k])
 
 
 class LSTM:
@@ -79,29 +81,35 @@ class LSTM:
     def __init__(self):
         self.params = Parameters()
 
-    def build_accum_model(self, n_steps):
+    def build_accum_model(self):
         self.params.w_x = initial_weights(self.n_cells, self.input_size)
         self.params.w_f = initial_weights(self.n_cells, self.input_size)
         self.params.w_i = initial_weights(self.n_cells, self.input_size)
-        #w_o = tt.matrix('W_o')
-        curr_s = self.s0 = tt.vector('s0')
 
-        def step(s, x):
-            c = tt.tanh(tt.tensordot(self.params.w_x, x, [[1], [0]]))
-            g_f =  tt.nnet.sigmoid(tt.tensordot(self.params.w_f, x, [[1], [0]]))
-            g_i =  tt.nnet.sigmoid(tt.tensordot(self.params.w_i, x, [[1], [0]]))
-            #g_o =  tt.nnet.sigmoid(tt.tensordot(w_o, x, [[0], [0]]))
+        self.s0 = tt.vector('s0')
+
+        def step(x, s):
+            c = tt.tanh(tt.tensordot(self.params.w_x, x,
+                                     [[1], [0]]))
+            g_f =  tt.nnet.sigmoid(tt.tensordot(self.params.w_f, x,
+                                                [[1], [0]]))
+            g_i =  tt.nnet.sigmoid(tt.tensordot(self.params.w_i, x,
+                                                [[1], [0]]))
 
             return c * g_i + s * g_f
 
+        self.x = tt.matrix('x')
+        self.n_steps = tt.iscalar('n_steps')
 
-        self.x = []
-        for i in range(n_steps):
-            curr_x = tt.vector('x%d' % i)
-            self.x.append(curr_x)
-            curr_s = step(curr_s, curr_x)
+        res, updates = theano.scan(
+            fn=step,
+            n_steps=self.n_steps,
+            outputs_info=self.s0,
+            sequences=[self.x]
+        )
+        res = res[-1]
 
-        return curr_s
+        return res + 0*self.n_steps
 
     def build_clf_model(self, curr_s):
         self.params.w_clf = initial_weights(self.clf_size, self.n_cells)
@@ -117,13 +125,14 @@ class LSTM:
 
 
 def update(params, grads, lr):
-    for p, g in zip(params.values(), grads):
-        p.set_value(p.get_value() - lr * g)
+    for i, p in enumerate(params.values()):
+        p.set_value(p.get_value() - lr * grads[i])
 
 
-def generate_data(n, n_steps):
+def generate_data(n):
     res = []
     for _ in range(n):
+        n_steps = random.randint(2, 5)
         # Toss a coin whether this is a positive or negative example.
         if random.random() > 0.5:
             # Positive.
@@ -131,14 +140,14 @@ def generate_data(n, n_steps):
             el = [[random.random(), random.random()]]
             x = el * n_steps
             y = np.asarray([1.0], dtype=np.float32)
-            res.append((np.asarray(x, dtype=np.float32), y))
+            res.append((np.asarray(x, dtype=np.float32), y, n_steps))
         else:
             # Negative.
             x = []
             for _ in range(n_steps):
                 x.append([random.random(), random.random()])
             y = np.asarray([0.0], dtype=np.float32)
-            res.append((np.asarray(x, dtype=np.float32), y))
+            res.append((np.asarray(x, dtype=np.float32), y, n_steps))
 
     return res
 
@@ -150,62 +159,79 @@ def main():
     n_steps = 5
     n_epochs = 1000
     # Generate some data.
-    data = generate_data(100, n_steps)
-    validation_data = generate_data(20, n_steps)
+    data = generate_data(100)
+    validation_data = generate_data(20)
 
     lstm = LSTM()
-    clf = lstm.build_clf_model(lstm.build_accum_model(n_steps))
-    f_clf = function([lstm.s0] + lstm.x, clf)
+    clf = lstm.build_clf_model(lstm.build_accum_model())
+    f_clf = function([lstm.s0, lstm.n_steps, lstm.x], clf)
+    """
+    x = np.ones((5, lstm.input_size), dtype=np.float32)
+    x[0,0] = 0
+    x[1,0] = 1
+    x[2,0] = 2
+    x[3,0] = 3
+    x[4,0] = 4
+    print f_clf(
+        np.zeros((lstm.n_cells, ), dtype=np.float32),
+        5,
+        x
+    )
+    return"""
+
+
     loss = lstm.build_loss(clf)
-    f_loss = function([lstm.s0] + lstm.x + [lstm.tgt], loss)
-
-
+    f_loss = function([lstm.s0, lstm.n_steps, lstm.x, lstm.tgt], loss)
 
     input = np.ndarray((lstm.input_size), dtype=np.float32)
     input[:] = 1.0
 
     loss_prime = theano.grad(loss, wrt=lstm.params.values())
-    f_loss_prime = function([lstm.s0] + lstm.x + [lstm.tgt], loss_prime)
+    f_loss_prime = function([lstm.s0, lstm.n_steps, lstm.x, lstm.tgt],
+                            loss_prime)
 
     for e in range(n_epochs):
         logging.info("Epoch #%d" % e)
-        g = None
-        for x, y in data:
-            args = [
-                np.zeros((lstm.n_cells, ), dtype=np.float32)
-            ]
-            for i in range(x.shape[0]):
-                args.append(x[i, :])
-            args.append(y)
-
-            g_point = f_loss_prime(*args)
-
-            if g is None:
-                g = np.zeros_like(g_point)
-            g += g_point
-
-        update(lstm.params, g, 0.1)
+        g = {}
 
         total_loss = 0.0
-        for i, (x, y) in enumerate(validation_data):
-            args = [
-                np.zeros((lstm.n_cells, ), dtype=np.float32)
-            ]
-            for i in range(x.shape[0]):
-                args.append(x[i, :])
-            args.append(y)
+        for x, y, n_steps in data:
+            g_point = f_loss_prime(
+                np.zeros((lstm.n_cells, ), dtype=np.float32),
+                n_steps,
+                x,
+                y)
+            total_loss += f_loss(
+                np.zeros((lstm.n_cells, ), dtype=np.float32),
+                n_steps,
+                x,
+                y) * 1.0 / len(data)
 
-            total_loss += f_loss(*args) * 1.0 / len(validation_data)
+            for i in range(len(g_point)):
+                if not i in g:
+                    g[i] = np.zeros_like(g_point[i], dtype=np.float32)
+
+            for i in range(len(g_point)):
+                g[i] += g_point[i] #* 1.0 / len(data)
+
+        validation_loss = 0.0
+        for i, (x, y, n_steps) in enumerate(validation_data):
+            args = [
+                np.zeros((lstm.n_cells, ), dtype=np.float32),
+                n_steps,
+                x,
+                y
+            ]
+            validation_loss += f_loss(*args) * 1.0 / len(validation_data)
 
             if e % 50 == 0:
                 logging.info("%d: tgt(%.0f) clf(%.2f)" % (i, y, f_clf(*args[
                                                                        :-1])))
 
-        logging.info("Validation loss: %.5f" % total_loss)
+        logging.info("train_loss(%.5f) valid_loss(%.5f)" % (
+            total_loss, validation_loss))
 
-
-
-
+        update(lstm.params, g, 0.1)
 
 
 if __name__ == '__main__':
